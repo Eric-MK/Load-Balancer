0e4c9930313e86c9b3f94630654f35ae56431a28
unknown <mutungaeric839@gmail.com>
2024-05-02 16:40:55 +0300

    load balancer first endpoint rep for getting replicas

--- load_balancer/load_balancer.py
+++ load_balancer/load_balancer.py
@@ -5,3 +5,25 @@ import random
 import requests
 import docker
 import re
+
+
+app = Flask(__name__)
+
+def update_server_containers():
+    global server_containers
+    containers = client.containers.list()
+    server_containers = [container.name for container in containers if 'server' in container.name.lower()]
+
+
+@app.route('/rep', methods=['GET'])
+def get_replicas():
+    # Optionally update the list on every request
+    update_server_containers()
+    
+    return jsonify({
+        "message": {
+            "N": len(server_containers),
+            "replicas": server_containers
+        },
+        "status": "successful"
+    }), 200
